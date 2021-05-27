2121c7dc94bf52beddb775cd28b3ec2635b26083
Balakrishna Saravanan Kesavan <bala@Balakrishnas-MacBook-Pro.local>
2021-05-27 11:18:28 +0530

    edited travis and deploy files

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t balawillgetyou/multi-client:latest -t balawillgetyou/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t balawillgetyou/multi-server:latest -t balawillgetyou/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t balawillgetyou/multi-worker:latest -t balawillgetyou/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push balawillgetyou/multi-client:latest
+docker push balawillgetyou/multi-server:latest
+docker push balawillgetyou/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push balawillgetyou/multi-client:$SHA
+docker push balawillgetyou/multi-server:$SHA
+docker push balawillgetyou/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=balawillgetyou/multi-server:$SHA
+kubectl set image deployments/client-deployment client=balawillgetyou/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=balawillgetyou/multi-worker:$SHA
